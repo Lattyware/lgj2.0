614e215111d1ccef1c8476af4eea9e3244edc8b4
Gareth Latty <gareth@lattyware.co.uk>
2012-03-25 06:15:47 +0100

    Random level generation, added more options.

--- Scripts/Level/Level.cs
+++ Scripts/Level/Level.cs
@@ -4,31 +4,35 @@ using System.Collections.Generic;
 
 public class Level : MonoBehaviour {
 
-	private static readonly int ISLANDS = 10;
+	private static readonly int ISLANDS = 8;
 	
 	public GameObject islandPrefab;
 	public GameObject seaPrefab;
 	
 	public void Start() {
+		Random.seed = (int) System.DateTime.Now.Ticks;
+		
 		List<GameObject> islands = new List<GameObject>();
 		GameObject sea = (GameObject) Instantiate(seaPrefab, new Vector3(0, 0, 0), Quaternion.identity); 
 		
-		Vector3 overlap = islandPrefab.renderer.bounds.size;
-		
+		Vector3 overlap = islandPrefab.renderer.bounds.size;		
 		Vector3 centre = sea.renderer.bounds.center;
 		Vector3 size = sea.renderer.bounds.size;
 		
-		SortedAreaStack areas = new SortedAreaStack(new Bounds(centre, size-overlap));
-		areas.repeatedSplit(Level.ISLANDS);
+		float error = 0.25f; //How varied the splits are, resulting in less gridy layouts. Higher -> Less on map. Lower -> Grid layout.
+		float border = 1.2f; //How much gap to leave around the edge to avoid falling off. Higher -> Less on map. Lower -> Islands falling off map.
 		
-		for (int i=0; i<Level.ISLANDS; i++) {
-			GameObject island = (GameObject) Instantiate(islandPrefab, areas.Pop().center, Quaternion.identity);
+		SortedAreaStack areas = new SortedAreaStack(new Bounds(centre, size-overlap*(border+error)));
+		areas.repeatedSplit(Level.ISLANDS, error); //Split to fit the given number of islands. Will fit more on, but may place islands on top of each other if too many requested.
+		//areas.splitUntil(error, islandPrefab.renderer.bounds.extents); //Split repeatedly until no more islands will fit. Will give up earlier.
+		
+		foreach (Bounds area in areas) {
+			GameObject island = (GameObject) Instantiate(islandPrefab, area.center, Quaternion.identity);
 			islands.Add(island);
 		}
-		
 	}
 	
-	private class SortedAreaStack {
+	private class SortedAreaStack : IEnumerable {
 		
 		private List<Bounds> stack;
 		
@@ -37,9 +41,35 @@ public class Level : MonoBehaviour {
 			this.stack.Add(initial);
 		}
 		
-		public void repeatedSplit(int times) {
+		public IEnumerator GetEnumerator() {
+			return this.stack.GetEnumerator();
+		}
+		
+		public void splitUntil(float error, Vector3 min) {
+			min *= (1+error);
+			
+			while (true) {
+				Bounds current = this.Pop();
+				Pair split = this.split(current, error);
+				
+				if (smallerThan(split.first, min) | smallerThan(split.second, min)) {
+					this.Add(current);
+					return;
+				}
+				
+				this.stack.Add(split.first);
+				this.stack.Add(split.second);
+				this.stack.Sort(compareAreasByArea);
+			}
+		}
+		
+		private bool smallerThan(Bounds current, Vector3 min) {
+			return current.extents.x < min.x | current.extents.z < min.z;
+		}
+		
+		public void repeatedSplit(int times, float error) {
 			for (int i=0; i<times; i++) {
-				Pair split = this.split(this.Pop(), 0.3f);
+				Pair split = this.split(this.Pop(), error);
 				this.stack.Add(split.first);
 				this.stack.Add(split.second);
 				this.stack.Sort(compareAreasByArea);
@@ -83,18 +113,17 @@ public class Level : MonoBehaviour {
 			float hw = area.extents.x;
 			float hh = area.extents.z;
 			float hd = area.extents.y;
-			error *= Random.Range(-1, 1);
+			float marginw = error*hw/2*Random.Range(-1, 1);
+			float marginh = error*hh/2*Random.Range(-1, 1);
 			if (hw > hh) {
-				error *= hw/2;
 				return new Pair(
-					new Bounds(new Vector3(centre.x-(hw/2)+error, centre.y, centre.z), new Vector3(hw+error, hd*2, hh*2)),
-					new Bounds(new Vector3(centre.x+(hw/2)-error, centre.y, centre.z), new Vector3(hw-error, hd*2, hh*2))
+					new Bounds(new Vector3(centre.x-(hw/2)+marginw, centre.y, centre.z-marginh), new Vector3(hw+marginw, hd*2, hh*2)),
+					new Bounds(new Vector3(centre.x+(hw/2)-marginw, centre.y, centre.z+marginh), new Vector3(hw-marginw, hd*2, hh*2))
 				);
 			} else {
-				error *= hh/2;
 				return new Pair(
-					new Bounds(new Vector3(centre.x, centre.y, centre.z-(hw/2)+error), new Vector3(hw*2, hd*2, hh+error)),
-					new Bounds(new Vector3(centre.x, centre.y, centre.z+(hw/2)-error), new Vector3(hw*2, hd*2, hh-error))
+					new Bounds(new Vector3(centre.x-marginw, centre.y, centre.z-(hw/2)+marginh), new Vector3(hw*2, hd*2, hh+marginh)),
+					new Bounds(new Vector3(centre.x+marginw, centre.y, centre.z+(hw/2)-marginh), new Vector3(hw*2, hd*2, hh-marginh))
 				);
 			}
 		} 
